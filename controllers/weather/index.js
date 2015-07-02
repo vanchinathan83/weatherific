dc41bf599b15952b04efab857414cc820104815d
vanchinathanac <ac.vanchinathan@gmail.com>
2015-07-01 23:55:38 -0500

    setting up the weather api

--- controllers/weather/index.js
+++ controllers/weather/index.js
@@ -4,6 +4,21 @@ var weather = require('../../lib/async_weather');
 
 module.exports = function(router) {
 	router.get('/', function(request,response){
-		response.send("Hello weather app");
+		var locations = [{"stateCode" : "CA", "city" : "Campbell"},{"stateCode" : "NE", "city" : "Omaha"}, {"stateCode" : "TX", "city" : "Austin"},{"stateCode" : "MD", "city" : "Timonium"}];
+		var apiEndpoints = [];
+		locations.forEach(function(location){
+			apiEndpoints.push(getAPIEndpoint(location,request));
+		});
+		weather.getAsyncWeather(apiEndpoints,function(results){
+			response.send(results);
+		});
 	});
 };
+
+/**
+Construct the API Endpoint URL 
+**/
+function getAPIEndpoint(location,request){
+	var baseUrl = request.app.kraken.get("api:wunderground:base_url") + request.app.kraken.get("api:wunderground:api_key") + "/" + request.app.kraken.get("api:wunderground:conditions_context") + "q/";
+	return baseUrl + location.stateCode + "/" + location.city + ".json";
+}
