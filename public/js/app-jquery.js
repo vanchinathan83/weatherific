7a788b81c8eced52358f1734f502c1c728f3c713
vanchinathanac <ac.vanchinathan@gmail.com>
2015-07-06 16:45:47 -0500

    Integrating bing search

--- public/js/app-jquery.js
+++ public/js/app-jquery.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+	$("#weatherCarousel").on("slide.bs.carousel", function(){
+		var classSelector = $('.item.active');
+		var index = $('.item.active').index() + 1;
+		if(index === 4){
+			index = 0;
+		}
+		var location = $("#item-" + index).text().split(',');
+		var city = location[0];
+		var state = location[1].slice(0,3).trim();
+		console.log(city);
+		console.log(state);
+		console.log(location);
+		var searchtext = (city + " " + state).replace(" " , "%27");
+		$.ajax({
+			type: 'GET',
+			url: "https://api.datamarket.azure.com/Bing/Search/Image?Query=%27+"+searchtext+"%27&$format=json",
+			headers : { 
+				"Authorization" : "Basic " + "OklsOXpXd0o1TVVZcUVrdTZZaUFMdDlnMzhtbEhLUzJQdmRLWHYwU1FmTlU="
+			}
+		}).done(function(data){
+			var html = "";
+			data.d.results.forEach(function(result){
+				html += "<a href=\"" + result.SourceUrl + "\"><img src=\"" + result.Thumbnail.MediaUrl +"\" width=100 height=100></a>";
+			});
+			$("#imageresults").html(html);
+		});
+	})
+	
 	$(".celcius").hide();
 	$(".toggler").click(function(){
 		if($(".celcius").css('display') === 'none'){
