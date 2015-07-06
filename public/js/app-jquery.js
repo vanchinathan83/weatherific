3838421a72e3c8e2c670f15033364e9754c1583f
vanchinathanac <ac.vanchinathan@gmail.com>
2015-07-06 12:01:32 -0500

    creating a F and C toggler

--- public/js/app-jquery.js
+++ public/js/app-jquery.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-	
+	$(".celcius").hide();
+	$(".toggler").click(function(){
+		if($(".celcius").css('display') === 'none'){
+			$(".toggler").text("Show temperature in Fahrenheit");
+		}
+		else{
+			$(".toggler").text("Show temperature in Celcius");
+		}
+		$(".celcius").toggle();
+		$(".fahrenheit").toggle();
+	});
+	$('#weatherCarousel').click(function() {
+  		
+	});
 });
+
+
+
+
